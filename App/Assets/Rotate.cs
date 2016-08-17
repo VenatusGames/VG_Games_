6078c5da122404765e4f15eb14d444f2a9fc2b5b
Lewis Savage <lewissavage88@gmail.com>
2016-08-17 18:01:09 +0100

    No real reason

--- App/Assets/Rotate.cs
+++ App/Assets/Rotate.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 
 public class Rotate : MonoBehaviour {
+	public int speed;
 	Vector3 vec = Vector3.one;
 	void Update () {
-		transform.Rotate(vec * 5);
+		transform.Rotate(vec * speed);
 	}
 }
