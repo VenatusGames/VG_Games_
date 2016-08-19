7c6b95b8e54305818e33a04292c0da68bc7a2627
Lewis Savage <lewissavage88@gmail.com>
2016-08-19 21:33:28 +0100

    Update number whatever the fuck

--- App/Assets/Fish.cs
+++ App/Assets/Fish.cs
@@ -2,14 +2,65 @@
 using System.Collections;
 
 public class Fish : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
+	Vector3 target;
+	public float speed;
+	public bool attracted;
+	public GameObject bobber;
+	public FishDatabase database;
+	public GameObject fishPrefab;
+	public bool caught;
+	void Start(){
+		StartCoroutine(FadeIn());
+		database = FindObjectOfType<FishDatabase>();
+		fishPrefab = database.fish[Random.Range(0,database.fish.Count)];
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update(){
+		if(!caught){
+			if(!attracted){
+				if(target != Vector3.zero){
+					Vector3 pos = transform.position;
+					speed = Random.Range(0f,1f);
+					float step = speed * Time.deltaTime;
+					transform.position = Vector3.MoveTowards(pos,target,step);
+					if(transform.position == target){
+						CancelInvoke();
+						RandomPosInSquare();
+					}
+				}else{
+					RandomPosInSquare();
+				}
+			}else if(bobber){
+				Vector3 pos = transform.position;
+				speed = Random.Range(0f,1f);
+				float step = speed * Time.deltaTime;
+				transform.position = Vector3.MoveTowards(pos,new Vector3(bobber.transform.position.x,1.14f,bobber.transform.position.z),step);
+			}
+		}else{
+			Vector3 pos = transform.position;
+			speed = Random.Range(0f,1f);
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(pos,new Vector3(bobber.transform.position.x,1.14f,bobber.transform.position.z),step);
+		}
+	}
+
+	void RandomPosInSquare(){
+		target = new Vector3(Random.Range(-2f,4f),1.14f,Random.Range(2f,9f));
+		Invoke("RandomPosInSquare",2f);
+
+	}
+
+	public void PrepareFishToReel(){
+		caught = true;
+	}
+
+	IEnumerator FadeIn(){
+		SpriteRenderer render = GetComponent<SpriteRenderer>();
+		for (int i = 0; i < 10; i++) {
+			Color newCol = render.color;
+			newCol.a += 0.1f;
+			render.color = newCol;
+			yield return new WaitForEndOfFrame();
+		}
 	}
 }
---
+++ App/Assets/FishSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishSpawner : MonoBehaviour {
+	public int maxFish;
+	public int curFish;
+
+	public GameObject fishPrefab;
+	void Start(){
+		SpawnLoop();
+	}
+
+	void SpawnLoop(){
+		if(curFish < maxFish && Random.Range(0,2) == 1){
+			Vector3 pos = new Vector3(Random.Range(-2f,4f),1.15f,Random.Range(2f,9f));
+			Instantiate(fishPrefab,pos,fishPrefab.transform.rotation);
+			curFish++;
+		}
+		Invoke("SpawnLoop",2);
+	}
+}
